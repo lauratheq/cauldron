6b2d968c14c5e996592f9dff5616fabdf6758a36
Herzog, Laura <laura.herzog@ionos.com>
2023-09-27 07:21:30 +0200

    Maintenance

--- cauldron/index.php
+++ cauldron/index.php
@@ -1,16 +1,23 @@
 <?php
 
+// set the absolute path to the system
+// and load the user config
 if ( ! defined( 'ABSPATH' ) )
 	define( 'ABSPATH', __DIR__ );
 require_once ABSPATH . '/config.php';
 
+// overwrite the debug mode
 if ( DEBUG_MODE === true ) {
 	ini_set( 'display_errors', 1 );
 	ini_set( 'display_startup_errors', 1 );
 	error_reporting( E_ALL );
 }
 
+// Load system required files
 require_once ABSPATH . '/src/Hooks.php';
 require_once ABSPATH . '/src/Helpers.php';
 require_once ABSPATH . '/src/DB.php';
 require_once ABSPATH . '/src/Options.php';
+
+// System is loaded, engage
+Hooks::exec( 'system_loaded' );
